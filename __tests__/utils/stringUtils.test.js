b0c18eb2016c15a825bb9b3c67a2c009fc126a3f
Arseny Ivasyuk <poiasd3241@gmail.com>
2021-06-17 00:23:38 +0300

    Fixes

--- __tests__/utils/stringUtils.test.js
+++ __tests__/utils/stringUtils.test.js
@@ -1,23 +1,17 @@
 const { test, expect, describe } = require("@jest/globals");
 const { isEmptyOrWhitespace } = require("../../src/utils/stringUtils");
 
-// describe.each(["", " ", "\n"])("The string is empty or whitespace.", (str) => {
-//   test(`'${str}'`, () => {
-//     expect(isEmptyOrWhitespace(str)).toBe(true);
-//   });
-// });
-
-// describe.each([" a", "a ", " a ", "a"])(
-//   "The string is not empty nor whitespace.",
-//   (str) => {
-//     test(`'${str}'`, () => {
-//       expect(isEmptyOrWhitespace(str)).toBe(false);
-//     });
-//   }
-// );
-
-test("asd", () => {
-  expect(isEmptyOrWhitespace("")).toBe(true);
-  expect(isEmptyOrWhitespace("   ")).toBe(true);
-  expect(isEmptyOrWhitespace("  a")).toBe(false);
+describe.each(["", " ", "\n"])("The string is empty or whitespace.", (str) => {
+  test(`'${str}'`, () => {
+    expect(isEmptyOrWhitespace(str)).toBe(true);
+  });
 });
+
+describe.each([" a", "a ", " a ", "a"])(
+  "The string is not empty nor whitespace.",
+  (str) => {
+    test(`'${str}'`, () => {
+      expect(isEmptyOrWhitespace(str)).toBe(false);
+    });
+  }
+);
--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -3,7 +3,7 @@
  * @returns True if the string is empty or whitespace; otherwise, false.
  */
 function isEmptyOrWhitespace(str) {
-  return str.length === 0 || (str.length !== 0 && str.trim() === "");
+  return str.length === 0 || str.trim() === "";
 }
 
 module.exports = { isEmptyOrWhitespace };
