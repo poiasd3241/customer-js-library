202054426b4365a85873c09184e7c7a26005bd06
Arseny Ivasyuk <poiasd3241@gmail.com>
2021-06-16 23:31:51 +0300

    Update stringUtils.test.js

--- __tests__/utils/stringUtils.test.js
+++ __tests__/utils/stringUtils.test.js
@@ -1,17 +1,23 @@
 const { test, expect, describe } = require("@jest/globals");
 const { isEmptyOrWhitespace } = require("../../src/utils/stringUtils");
 
-describe.each(["", " ", "\n"])("The string is empty or whitespace.", (str) => {
-  test(`'${str}'`, () => {
-    expect(isEmptyOrWhitespace(str)).toBe(true);
-  });
-});
+// describe.each(["", " ", "\n"])("The string is empty or whitespace.", (str) => {
+//   test(`'${str}'`, () => {
+//     expect(isEmptyOrWhitespace(str)).toBe(true);
+//   });
+// });
 
-describe.each([" a", "a ", " a ", "a"])(
-  "The string is not empty nor whitespace.",
-  (str) => {
-    test(`'${str}'`, () => {
-      expect(isEmptyOrWhitespace(str)).toBe(false);
-    });
-  }
-);
+// describe.each([" a", "a ", " a ", "a"])(
+//   "The string is not empty nor whitespace.",
+//   (str) => {
+//     test(`'${str}'`, () => {
+//       expect(isEmptyOrWhitespace(str)).toBe(false);
+//     });
+//   }
+// );
+
+test("asd", () => {
+  expect(isEmptyOrWhitespace("")).toBe(true);
+  expect(isEmptyOrWhitespace("   ")).toBe(true);
+  expect(isEmptyOrWhitespace("  a")).toBe(false);
+});
